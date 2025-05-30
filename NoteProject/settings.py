82530a2d23528563eb2bb0ea1c9e9bfbd484f7d6
Eduard Prystupa <eduard.prystupa@gmail.com>
2025-05-30 13:38:47 +0300

    database env

--- NoteProject/settings.py
+++ NoteProject/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'NoteProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 # Password validation
