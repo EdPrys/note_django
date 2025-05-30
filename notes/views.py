fc818b4508e787cec53dcc1148ee450602655be2
Eduard Prystupa <eduard.prystupa@gmail.com>
2025-05-30 14:42:34 +0300

    add JSONparser

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from .models import Note
@@ -22,7 +22,7 @@ class NoteListAPIView(ListAPIView):
 class NoteCreateAPIView(CreateAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
-    parser_classes = [MultiPartParser, FormParser]
+    parser_classes = [MultiPartParser, FormParser, JSONParser]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -32,6 +32,7 @@ class NoteDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser, JSONParser]
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
